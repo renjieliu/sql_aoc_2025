fb8728aeb5161358eb872ae758c775fb1104138e
Renjie Liu <anlrj@qq.com>
2025-12-03 16:07:59 -0500

    .

--- Day_03_2nd_in_Python.py
+++ Day_03_2nd_in_Python.py
@@ -24,29 +24,67 @@ with open("Day_03_input.txt") as f:
 
 
 
-def find_max(s, n):
-    stk = [] 
-    remove = len(s) - n
+# def find_max(s, n):
+#     stk = [] 
+#     remove = len(s) - n
 
-    for c in s:
-        while remove > 0 and stk and stk[-1] < c:
-            remove -=1
-            stk.pop()
+#     for c in s:
+#         while remove > 0 and stk and stk[-1] < c:
+#             remove -=1
+#             stk.pop()
         
-        stk.append(c)
+#         stk.append(c)
 
-    return int(''.join(stk[:n]))  #only return the first n characters
+#     return int(''.join(stk[:n]))  #only return the first n characters
 
-# print(find_max(s, 12))
+# # print(find_max(s, 12))
 
-output = 0
-for line in s:
-    line = line.replace('\n', '')
-    output += find_max(line, 12)
+# output = 0
+# for line in s:
+#     line = line.replace('\n', '')
+#     output += find_max(line, 12)
 
-print(output)
+# print(output)
 
 
     
 
-        
+def find_max_dp(s, need):
+    total_length = len(s) 
+
+    dp = [["" for _ in range(need+1)] for _ in range(total_length+1)] 
+
+    for pos in range(total_length-1, -1, -1):
+        for to_take in range(1, need+1):
+            if pos + to_take > total_length: 
+                continue
+            A = dp[pos+1][to_take]
+            B = s[pos] + dp[pos+1][to_take-1]
+            
+            dp[pos][to_take] = max(A, B)
+
+    return int(dp[0][need])
+
+
+
+
+    # s_length = len(s)
+
+    # dp = [[""] * (need + 1) for _ in range(s_length + 1)]
+    
+    # for pos in range(s_length-1, -1, -1):
+    #     for to_take in range(1, need + 1):
+    #         if s_length - pos >= to_take: 
+    #             option_skip = dp[pos + 1][to_take]
+    #             option_take = s[pos] + dp[pos + 1][to_take - 1]
+    #             dp[pos][to_take] = max(option_take, option_skip)
+
+    # return int(dp[0][need])
+
+output = 0
+for line in s:
+    line = line.replace('\n', '')
+    output += find_max_dp(line, 12)
+
+print(output)
+
