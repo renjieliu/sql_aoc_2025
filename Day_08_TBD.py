c28786dcc1a5526af36af2720f305e6ecd5d9e1c
Renjie Liu <anlrj@qq.com>
2025-12-08 18:20:14 -0500

    .

---
+++ Day_08_TBD.py
@@ -0,0 +1,82 @@
+with open('Day_08_input.txt') as f:
+    input = f.readlines()
+    
+arr = [i.replace("\n", "") for i in input]
+pick = 1000
+cost = []
+
+for i in range(len(arr)-1):
+    for j in range(i+1, len(arr)):
+        A = [int(_) for _ in arr[i].split(",") ]  
+        B = [int(_) for _ in arr[j].split(",") ] 
+        
+        C = (B[0]-A[0])**2 + (B[1]-A[1])**2 +(B[2]-A[2])**2
+        cost.append((i+1, j+1, C)) 
+
+cost = sorted(cost, reverse=0, key = lambda x: x[2])
+
+parent = {}
+size = {}
+
+
+def find(a): # to find parent of a
+    if parent[a]!=a:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+    
+def union(a, b):
+    if a not in parent:
+        parent[a] = a 
+        size[a] = 1
+
+    if b not in parent:
+        parent[b] = b 
+        size[b] = 1
+
+    root_a = find(a)
+    root_b = find(b)
+    
+    if root_a != root_b:
+        if size[root_a] >= size[root_b]:
+            parent[root_b] = root_a 
+            size[root_a] += size[root_b] 
+        
+        elif size[root_a] < size[root_b]:
+            parent[root_a] = root_b 
+            size[root_b] +=  size[root_a]
+        
+    
+for a, b, c in cost[:pick]:
+    union(a, b)
+
+leaf = {}
+
+for a, b, c in cost[:pick]:
+    p_a = find(a)
+    p_b = find(b) 
+    
+    if p_a not in leaf:
+        leaf[p_a] = set() 
+    leaf[p_a].add(a)
+    if p_b not in leaf:
+        leaf[p_b] = set() 
+    leaf[p_b].add(b)
+
+
+lst = sorted ( [len(v) for k, v in leaf.items() ], reverse=1)  
+
+print(lst)
+
+
+print('ans1: ', lst[0]*lst[1]*lst[2]) 
+
+
+
+
+# part 2: to be done. need to find the point where all the 1000 boxes are connected into one tree
+
+
+
+
+
